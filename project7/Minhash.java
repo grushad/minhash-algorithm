db0ed75aafdc779c669396302053ebab9dc88bf6
grushad <grushadharod@gmail.com>
2023-06-10 16:51:15 -0700

    first commit

---
+++ project7/Minhash.java
@@ -0,0 +1,21 @@
+package project7;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.ArrayList;
+
+public class Minhash {
+  
+  public double jaccard(String fA, String fB) {
+    /**
+     * fA: Name of first file
+     * fB: Name of second file
+     */
+
+    // Your code goes here 
+
+    
+    return 0.0;
+  }
+
+}
