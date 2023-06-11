c0a0b0f276b3db0f647b2e21bc1e2564ec27191c
Grusha Dharod <grushadharod@gmail.com>
2023-06-10 22:33:07 -0700

    Update Minhash.java

--- project7/Minhash.java
+++ project7/Minhash.java
@@ -102,7 +102,7 @@ public class Minhash {
      * fB: Name of second file
      */  
     // Your code goes here 
-    int numHashFunc = 89, numFiles = 2;     
+    int numHashFunc = 197, numFiles = 2;     
     String content1 = readFile(fA);    
     String content2 = readFile(fB);
 
