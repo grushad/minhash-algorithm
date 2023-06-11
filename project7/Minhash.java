6c6dbfac2776c543382b6ad30943c9e070e24546
grushad <grushadharod@gmail.com>
2023-06-10 21:41:18 -0700

    followed blog impl

--- project7/Minhash.java
+++ project7/Minhash.java
@@ -9,57 +9,97 @@ import java.io.*;
 
 public class Minhash {
 
-  public Set<String> generateShingles(String content, int nGram){
-    Set<String> shingles = new HashSet<>();        
-    for(int i = 0; i <= content.length() - nGram; i++){
-      String shingle = content.substring(i, i + nGram);      
-      shingles.add(shingle);
-    }    
-    return shingles;
+  public void generateShingles(String content, Set<Integer> shingles){
+    int nGram = 2;
+    //Set<String> shingles = new HashSet<>();   
+    String[] arr = content.split(" ");
+    for(int i = 0; i < arr.length - nGram; i++){
+      String shingle = arr[i] +  " " + arr[i + 1] + " " + arr[i + 2];//content.substring(i, i + nGram);      
+      shingles.add(shingle.hashCode());
+    }
+    //return shingles;
   }
 
-  public int[] generateHashFunc(int nGram, int numHashFunc){
+  public int[] generateHashFunc(int numHashFunc){
+    int limit = Integer.MAX_VALUE;
     int[] hashFunc = new int[numHashFunc];
     Random rand = new Random();
-
+    Set<Integer> uniq = new HashSet<>();
     for(int i = 0; i < numHashFunc; i++){
-      int a = rand.nextInt(nGram) + 1;
-      int b = rand.nextInt(nGram) + 1;
-      hashFunc[i] = a * b;
+      int a = rand.nextInt(limit);
+      while(uniq.contains(a)){
+        a = rand.nextInt(limit);
+      }
+      uniq.add(a);
+      hashFunc[i] = a;
     }
     return hashFunc;
   }
 
   public double calcJSim(String content1, String content2, int numHashFunc, int nGram, int numFiles){
     
-    Set<String> set1 = generateShingles(content1, nGram);
-    Set<String> set2 = generateShingles(content2, nGram);
+    Set<Integer> set1 = new HashSet<>();
+    generateShingles(content1, set1);
+    System.out.println(set1);
+    
+    Set<Integer> set2 = new HashSet<>();
+    generateShingles(content2, set2);
+    System.out.println(set2);    
 
-    Set<String> allShingles = new HashSet<>();
-    allShingles.addAll(set1);
-    allShingles.addAll(set2);
+   //Set<String> allShingles = new HashSet<>();
+    // allShingles.addAll(set1);
+    // allShingles.addAll(set2);
 
-    int sz = allShingles.size();
+    // int sz = shingles.size();
+    // System.out.println(sz);
 
-    int[] hashFunc = generateHashFunc(sz, numHashFunc);
+    int[] aCoeff = generateHashFunc(numHashFunc);
+    int[] bCoeff = generateHashFunc(numHashFunc);
+
     int[][] signatures = new int[numHashFunc][numFiles];
 
-    for(int[] row: signatures){
-      row[0] = Integer.MAX_VALUE;
-      row[1] = Integer.MAX_VALUE;
-    }
+    // for(int[] row: signatures){
+    //   row[0] = Integer.MAX_VALUE;
+    //   row[1] = Integer.MAX_VALUE;
+    // }
 
-    for(String shingle: allShingles){
-      int hashCode = shingle.hashCode();
-      for(int i = 0; i < numHashFunc; i++){
-        int hashValue = (hashFunc[i] * hashCode) % sz;
-        if(hashValue < signatures[i][0]){
-          signatures[i][0] = hashValue;
-        }
-        if(hashValue < signatures[i][1]){
-          signatures[i][1] = hashValue;
+    int prime = 42949673;
+
+    // for(String shingle: allShingles){
+    //   int hashCode = shingle.hashCode();
+    //   // System.out.println(hashCode);
+    //   for(int i = 0; i < numHashFunc; i++){
+    //     int hashValue = (hashFunc[i] * hashCode) % prime; // % sz
+    //     // System.out.println(hashValue);
+    //     if(hashValue < signatures[i][0]){
+    //       signatures[i][0] = hashValue;
+    //     }
+    //     if(hashValue < signatures[i][1]){
+    //       signatures[i][1] = hashValue;
+    //     }
+    //   }
+    // }
+    // int minHashCode = prime + 1;
+    for(int j = 0; j < numHashFunc; j++){
+      int minHashCode = prime + 1;
+      for(Integer shingle: set1){
+        int hashCode = (aCoeff[j] * shingle + bCoeff[j]) % prime;
+        if(hashCode < minHashCode){
+          minHashCode = hashCode;
         }
       }
+      signatures[j][0] = minHashCode;
+    }
+
+    for(int j = 0; j < numHashFunc; j++){
+      int minHashCode = prime + 1;
+      for(Integer shingle: set2){
+        int hashCode = (aCoeff[j] * shingle + bCoeff[j]) % prime;
+        if(hashCode < minHashCode){
+          minHashCode = hashCode;
+        }
+      }
+      signatures[j][1] = minHashCode;
     }
 
     int matches = 0;
@@ -68,6 +108,7 @@ public class Minhash {
         matches++;
       }
     }
+    // System.out.println(matches);
     return (double)matches / (double)numHashFunc;
   }
 
@@ -77,7 +118,7 @@ public class Minhash {
       File myObj = new File(fileName);
       Scanner myReader = new Scanner(myObj);
       while (myReader.hasNextLine()) {
-        sb.append(myReader.nextLine());        
+        sb.append(myReader.nextLine() + " ");        
       }
       myReader.close();
     } catch (FileNotFoundException e) {
